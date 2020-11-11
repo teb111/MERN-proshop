78b8f0a32fc817cc52e426a3819491fc8e93e624
Tobiloba <tobiloba.ft@gmail.com>
2020-11-11 04:10:06 +0000

    code addded

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
+// importing components
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
 
 function App() {
   return (
     <>
-      <h1>Welcome to proshop</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to proshop</h1>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 }
---
+++ frontend/src/components/Product.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+import Rating from "./Rating";
+
+const Product = ({ product }) => {
+  return (
+    <Card className='my-3 p-3 rounded'>
+      <a href={`/product/${product._id}`}>
+        <Card.Img src={product.image} variant='top' />
+      </a>
+
+      <Card.Body as='div'>
+        <a href={`/product/${product._id}`}>
+          <strong>{product.name}</strong>
+        </a>
+
+        <Card.Text as='div'>
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
+        </Card.Text>
+
+        <Card.Text as='h3'>${product.price}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Product;
