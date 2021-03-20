288102dbf43e73d47d4d258ad030b9caba877ea9
unknown <fakoredetobiloba.ft@gmail.com>
2021-03-20 09:24:58 -0400

    lesson 40 Implenting Jsonwebtoken and adding protected routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import asyncHandler from "express-async-handler";
+import generateToken from "../utils/generateToken.js";
 import User from "../models/userModel.js";
 
 // @desc     Auth user and get token
@@ -16,7 +17,7 @@ const authUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null,
+      token: generateToken(user._id), // generating a token alongside the user's id
     });
   } else {
     res.status(401);
@@ -24,4 +25,23 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser };
+// @desc     Get user profile
+// @route    GET /api/users/profile
+// @access   Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id); // req.user from our middleware "protect" in our userroutes
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User Not Found");
+  }
+});
+
+export { authUser, getUserProfile };
---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,34 @@
+import jwt from "jsonwebtoken";
+import asyncHandler from "express-async-handler"; // to handle any unHandled promise rejection warnings
+import User from "../models/userModel.js";
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1]; // Grabbing only our token here alone and leaving the "Bearer"
+
+      const decoded = jwt.verify(token, process.env.JWT_SECRET); // and verifying it with our secret JWT and we are getting something like
+      // { id: '601f29cd01dda224a093s7as07', iat: 1616244679, exp: 1618836679 }
+
+      req.user = await User.findById(decoded.id).select("-password"); // getting the user from the database but excluding the user's password
+
+      next();
+    } catch (error) {
+      console.log(error);
+      res.status(401);
+      throw new Error("Not authorized, token failed");
+    }
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized, no token");
+  }
+});
+
+export { protect };
