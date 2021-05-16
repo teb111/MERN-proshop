7bb0acc34be4fa9804b45f72e7d642b0926296a7
Fakorede Oluwatobiloba <fakoredetobiloba.ft@gmail.com>
2021-05-16 15:54:15 -0400

    added container in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -35,48 +35,54 @@ function App() {
     <Router>
       <Header />
       <main className="py-3">
-        {!userInfo && <Redirect to="/login" />}
-        <Switch>
-          <Route path="/shipping" component={ShippingScreen} exact />
-          <Route path="/payment" component={PaymentScreen} exact />
-          <Route path="/placeorder" component={PlaceOrderScreen} exact />
-          <Route path="/order/:id" component={OrderScreen} exact />
-          <Route path="/login" component={LoginScreen} exact />
-          <Route path="/register" component={RegisterScreen} exact />
-          <Route path="/profile" component={ProfileScreen} exact />
-          <Route path="/product/:id" component={ProductScreen} exact />
-          {/* so we are setting the id to be optional here by adding the question marks because wen we just go to the
+        <Container>
+          {!userInfo && <Redirect to="/login" />}
+          <Switch>
+            <Route path="/shipping" component={ShippingScreen} exact />
+            <Route path="/payment" component={PaymentScreen} exact />
+            <Route path="/placeorder" component={PlaceOrderScreen} exact />
+            <Route path="/order/:id" component={OrderScreen} exact />
+            <Route path="/login" component={LoginScreen} exact />
+            <Route path="/register" component={RegisterScreen} exact />
+            <Route path="/profile" component={ProfileScreen} exact />
+            <Route path="/product/:id" component={ProductScreen} exact />
+            {/* so we are setting the id to be optional here by adding the question marks because wen we just go to the
           cart it wont have any id except we to it through the productScreen */}
-          <Route path="/cart/:id?" component={CartScreen} exact />
-          <Route path="/admin/userlist" component={UserListScreen} exact />
-          <Route path="/admin/user/:id/edit" component={UserEditScreen} exact />
-          <Route
-            path="/admin/productlist"
-            component={ProductListScreen}
-            exact
-          />
-          <Route
-            path="/admin/productlist/:pageNumber"
-            component={ProductListScreen}
-            exact
-          />
-          <Route
-            path="/admin/product/:id/edit"
-            component={ProductEditScreen}
-            exact
-          />
-          <Route path="/admin/orderlist" component={OrderListScreen} exact />
-          <Route path="/search/:keyword" component={HomeScreen} exact />
-          <Route path="/page/:pageNumber" component={HomeScreen} />
-          <Route
-            path="/search/:keyword/page/:pageNumber"
-            component={HomeScreen}
-          />
-          <Route path="/" component={HomeScreen} exact />
-          {/* So basically what i am saying here is that i am settting the route for the homescreen which is "/"
+            <Route path="/cart/:id?" component={CartScreen} exact />
+            <Route path="/admin/userlist" component={UserListScreen} exact />
+            <Route
+              path="/admin/user/:id/edit"
+              component={UserEditScreen}
+              exact
+            />
+            <Route
+              path="/admin/productlist"
+              component={ProductListScreen}
+              exact
+            />
+            <Route
+              path="/admin/productlist/:pageNumber"
+              component={ProductListScreen}
+              exact
+            />
+            <Route
+              path="/admin/product/:id/edit"
+              component={ProductEditScreen}
+              exact
+            />
+            <Route path="/admin/orderlist" component={OrderListScreen} exact />
+            <Route path="/search/:keyword" component={HomeScreen} exact />
+            <Route path="/page/:pageNumber" component={HomeScreen} />
+            <Route
+              path="/search/:keyword/page/:pageNumber"
+              component={HomeScreen}
+            />
+            <Route path="/" component={HomeScreen} exact />
+            {/* So basically what i am saying here is that i am settting the route for the homescreen which is "/"
           so anytime someone goes to that route i want to render the homescreen but by default this will also render out
           anything above "/" like "/products/pages" so the "exact" there is to match that specific route alone */}
-        </Switch>
+          </Switch>
+        </Container>
       </main>
       {/* <Footer /> */}
     </Router>
